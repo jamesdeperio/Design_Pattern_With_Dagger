3f860451733ac47945dad9900be3e26bb3d16284
James de Perio <deperiojames20@gmail.com>
2018-12-07 16:48:14 +0800

    Update LoginEval.kt

--- app/src/main/java/io/github/jamesdeperio/architecture/appmodule/fragment/login/LoginEval.kt
+++ app/src/main/java/io/github/jamesdeperio/architecture/appmodule/fragment/login/LoginEval.kt
@@ -7,7 +7,7 @@ package io.github.jamesdeperio.architecture.appmodule.fragment.login
 import io.github.jamesdeperio.architecture.global.model.temp.login.LoginRequest
 
 /*
-    interpreter evaluation
+    interpreter's evaluation
  */
 class LoginEval : HasLoginContract.State {
     override fun isCredentialEmpty(username: String, password: String): Boolean
